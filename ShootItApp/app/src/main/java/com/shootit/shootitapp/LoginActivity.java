ee1365a0b8e267a01e8fca68cadf3d392b1a8d16
sean-mcgregor <worksmcg@gmail.com>
2021-12-23 12:26:08 +0000

    Successful creation of user with Firebase auth

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/LoginActivity.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/LoginActivity.java
@@ -1,33 +1,63 @@
 package com.shootit.shootitapp;
 
-import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    // Initialize Firebase Auth
+    private String email = "seanmcg490@gmail.com";
+    private String password = "password123";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance()
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+//                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+//                    updateUI(null);
+                }
+            }
+        });
     }
 
 
     @Override
     public void onStart() {
         super.onStart();
+
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
         if(currentUser != null){
-            reload();
+            System.out.println("User logged in");
         }
     }
 }
