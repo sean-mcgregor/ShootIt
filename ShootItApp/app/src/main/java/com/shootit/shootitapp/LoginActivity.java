e6b7e7bd0cf3713a90bad142d007d94a26bd810d
sean-mcgregor <worksmcg@gmail.com>
2021-12-23 11:55:10 +0000

    Moved google services file

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/LoginActivity.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/LoginActivity.java
@@ -4,11 +4,30 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FirebaseAuth mAuth;
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
     }
 }
