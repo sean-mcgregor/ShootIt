faa681fc13b2a7c77ad1204ca57adb2271be053c
sean-mcgregor <worksmcg@gmail.com>
2022-02-16 16:49:42 +0000

    Added functional map

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/MapFragment.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/MapFragment.java
@@ -5,9 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import androidx.fragment.app.Fragment;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     public MapFragment(){
         // require a empty public constructor
@@ -16,6 +24,22 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
         return inflater.inflate(R.layout.fragment_map, container, false);
+
+
+    }
+
+    // Get a handle to the GoogleMap object and display marker.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
