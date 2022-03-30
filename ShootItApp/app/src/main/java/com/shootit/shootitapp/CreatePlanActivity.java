62e297bf8b95ccdccab0d5c22de0c6a3a3ef76cc
sean-mcgregor <worksmcg@gmail.com>
2022-03-30 19:43:31 +0100

    Changed date picker structure so values selected can be accessed

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePlanActivity.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePlanActivity.java
@@ -6,6 +6,7 @@ import android.app.TimePickerDialog;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -17,11 +18,12 @@ import androidx.fragment.app.DialogFragment;
 
 public class CreatePlanActivity extends AppCompatActivity{
 
-    private TextView locationNameTextView;
+    private TextView locationNameTextView, dateTextView, timeTextView;
     private PhotoFragment photoFragment;
     private Button backButton, confirmButton, dateButton, timeButton;
     private ShootLocation location;
-
+    private DatePickerDialog datePickerDialog;
+    private int chosenMinute, chosenHour, chosenDay, chosenMonth, chosenYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +31,11 @@ public class CreatePlanActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_plan);
 
+        initDatePicker();
+
         locationNameTextView = (TextView) findViewById(R.id.locationNameTextView);
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        timeTextView = (TextView) findViewById(R.id.timeTextView);
 
         backButton = (Button) findViewById(R.id.back_button);
         confirmButton = (Button) findViewById(R.id.confirm_button);
@@ -67,8 +73,7 @@ public class CreatePlanActivity extends AppCompatActivity{
             @Override
             public void onClick(View view) {
 
-                DialogFragment newFragment = new DatePickerFragment();
-                newFragment.show(getSupportFragmentManager(), "datePicker");
+                datePickerDialog.show();
             }
         });
 
@@ -76,51 +81,54 @@ public class CreatePlanActivity extends AppCompatActivity{
             @Override
             public void onClick(View view) {
 
-                DialogFragment newFragment = new TimePickerFragment();
-                newFragment.show(getSupportFragmentManager(), "timePicker");
+//                DialogFragment newFragment = new TimePickerFragment();
+//                newFragment.show(getSupportFragmentManager(), "timePicker");
             }
         });
     }
 
 
-    // Static class for Date Picker
-    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    private void initDatePicker() {
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
 
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-        }
+                StringBuilder sb = new StringBuilder();
+                sb.append(day).append("/");
+                sb.append(month+1).append("/");
+                sb.append(year);
+                dateTextView.setText(sb.toString());
+            }
+        };
 
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-        }
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
     }
 
-
-    // Static class for Time Picker
-    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
-        }
-    }
+//    // Static class for Time Picker
+//    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+//
+//        @Override
+//        public Dialog onCreateDialog(Bundle savedInstanceState) {
+//            // Use the current time as the default values for the picker
+//            final Calendar c = Calendar.getInstance();
+//            int hour = c.get(Calendar.HOUR_OF_DAY);
+//            int minute = c.get(Calendar.MINUTE);
+//
+//            // Create a new instance of TimePickerDialog and return it
+//            return new TimePickerDialog(getActivity(), this, hour, minute,
+//                    DateFormat.is24HourFormat(getActivity()));
+//        }
+//
+//        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//            // Do something with the time chosen by the user
+//        }
+//    }
 }
