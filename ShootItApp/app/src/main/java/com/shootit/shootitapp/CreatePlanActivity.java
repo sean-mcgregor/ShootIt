bf80a41242a7fc46ada195611cdecaf5bd3ff01a
sean-mcgregor <worksmcg@gmail.com>
2022-03-30 17:38:09 +0100

    Added date and time picker static fragments

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePlanActivity.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePlanActivity.java
@@ -1,17 +1,25 @@
 package com.shootit.shootitapp;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class CreatePlanActivity extends AppCompatActivity{
 
     private TextView locationNameTextView;
     private PhotoFragment photoFragment;
-    private Button backButton, confirmButton;
+    private Button backButton, confirmButton, dateButton, timeButton;
     private ShootLocation location;
 
 
@@ -22,8 +30,11 @@ public class CreatePlanActivity extends AppCompatActivity{
         setContentView(R.layout.activity_create_plan);
 
         locationNameTextView = (TextView) findViewById(R.id.locationNameTextView);
+
         backButton = (Button) findViewById(R.id.back_button);
         confirmButton = (Button) findViewById(R.id.confirm_button);
+        dateButton = (Button) findViewById(R.id.dateButton);
+        timeButton = (Button) findViewById(R.id.timeButton);
 
         // Get location for plan
         if(getIntent().getExtras() != null) {
@@ -51,5 +62,65 @@ public class CreatePlanActivity extends AppCompatActivity{
                 finish();
             }
         });
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+            }
+        });
+    }
+
+
+    // Static class for Date Picker
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
+
+
+    // Static class for Time Picker
+    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
     }
 }
