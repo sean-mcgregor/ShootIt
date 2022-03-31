35b37e4ce495509a21957e48708f7a226c68cc3d
sean-mcgregor <worksmcg@gmail.com>
2022-03-31 21:08:49 +0100

    Create Plan Activity now ends with finish() upon press of confirm button

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePlanActivity.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePlanActivity.java
@@ -84,6 +84,8 @@ public class CreatePlanActivity extends AppCompatActivity{
 
                     pushPlanToDatabase();
                 }
+
+                finish();
             }
         });
 
