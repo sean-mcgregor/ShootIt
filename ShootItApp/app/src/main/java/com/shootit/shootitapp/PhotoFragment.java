a4d409cd34edf58c762d83c5da10f5970f4fa60d
sean-mcgregor <worksmcg@gmail.com>
2022-04-02 14:46:07 +0100

    Added cancel button to image popup and changed layout and style

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/PhotoFragment.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/PhotoFragment.java
@@ -5,12 +5,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -104,25 +106,54 @@ public class PhotoFragment extends Fragment {
         return v;
     }
 
+
+    // Function to instantiate and display dialog builder for fullscreen photo viewing
     public void showImagePopup() {
-        Dialog builder = new Dialog(getContext(), android.R.style.Theme_Light);
+
+        // Crate and configure dialog builder
+        Dialog builder = new Dialog(getContext(), android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
         builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
         builder.getWindow().setBackgroundDrawable(
-                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                new ColorDrawable(getResources().getColor(R.color.design_default_color_on_primary)));
+
+        // Destroy dialog if dismissed
         builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
             @Override
             public void onDismiss(DialogInterface dialogInterface) {
-                //nothing;
+
+                builder.cancel();
             }
         });
 
+        // Create imageview and load photo with glide
         ImageView imageView = new ImageView(getContext());
-
-        // Load photo from URI into image container
         Glide.with(getContext()).load(photoUri).into(imageView);
+
+        // Create and configure cancel button
+        Button cancelButton = new Button(getContext());
+        cancelButton.setBackgroundColor(getResources().getColor(R.color.BACK_BUTTON));
+        cancelButton.setText("Back");
+
+        // If cancel button clicked then destroy dialog
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                builder.cancel();
+            }
+        });
+
+        // Add button and imageview to builder
+        builder.addContentView(cancelButton, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+
         builder.addContentView(imageView, new RelativeLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // Display builder
         builder.show();
     }
 
