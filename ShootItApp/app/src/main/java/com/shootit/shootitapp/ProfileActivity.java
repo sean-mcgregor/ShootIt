19f97b8383a7372f20a9fcba3cef1ca9e4c7e418
sean-mcgregor <worksmcg@gmail.com>
2022-04-02 21:22:03 +0100

    Sucessfully logging out user and rebooting app on login activity

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/ProfileActivity.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/ProfileActivity.java
@@ -1,7 +1,17 @@
 package com.shootit.shootitapp;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ContentProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,12 +24,14 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.jakewharton.processphoenix.ProcessPhoenix;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    // Initialize Firebase Auth
+    private FirebaseAuth auth;
     private FirebaseUser user;
     private DatabaseReference userRef;
+    private Button logoutButton, deleteAccountButton;
 
     TextView emailText, usernameText;
 
@@ -30,13 +42,50 @@ public class ProfileActivity extends AppCompatActivity {
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         userRef = FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid());
+        auth = FirebaseAuth.getInstance();
 
         emailText = findViewById(R.id.emailText);
         usernameText = findViewById(R.id.usernameText);
+        logoutButton = findViewById(R.id.logout_button);
+        deleteAccountButton = findViewById(R.id.deleteAccountButton);
+
+        FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null){
+
+                    Log.d("Logged out", "success");
+                    // Once signout is complete, reboot application to login screen
+                    Intent intent = buildIntent();
+                    ProcessPhoenix.triggerRebirth(getApplicationContext(), intent);
+                }
+                else {
+
+                    Log.d("Logged out", "failure");
+                }
+            }
+        };
+
+        auth.addAuthStateListener(authStateListener);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
 
         updateUI();
     }
 
+    private Intent buildIntent() {
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        return intent;
+    }
+
+    
     private void updateUI() {
 
         StringBuilder emailField = new StringBuilder();
