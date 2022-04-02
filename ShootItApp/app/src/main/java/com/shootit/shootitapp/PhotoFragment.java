762a89841ca55116dfe16f7dba779e877083df57
sean-mcgregor <worksmcg@gmail.com>
2022-04-02 12:47:24 +0100

    Image popup now working in all contexts with glide

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/PhotoFragment.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/PhotoFragment.java
@@ -1,14 +1,19 @@
 package com.shootit.shootitapp;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
@@ -84,9 +89,41 @@ public class PhotoFragment extends Fragment {
                     dialog.show();
                 }
             });
+        } else {
+
+            // If user clicks photo fragment
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    showImagePopup();
+                }
+            });
         }
 
         return v;
+    }
+
+    public void showImagePopup() {
+        Dialog builder = new Dialog(getContext(), android.R.style.Theme_Light);
+        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        builder.getWindow().setBackgroundDrawable(
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                //nothing;
+            }
+        });
+
+        ImageView imageView = new ImageView(getContext());
+
+        // Load photo from URI into image container
+        Glide.with(getContext()).load(photoUri).into(imageView);
+        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        builder.show();
     }
 
 }
