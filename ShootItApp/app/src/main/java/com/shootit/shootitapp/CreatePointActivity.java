9e1b26902baab26dc50985cfcea85377d57abcd3
sean-mcgregor <worksmcg@gmail.com>
2022-04-04 00:53:26 +0100

    Cleaned code

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePointActivity.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/CreatePointActivity.java
@@ -1,30 +1,18 @@
 package com.shootit.shootitapp;
 
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -35,10 +23,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -57,7 +42,7 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
     private FirebaseUser user;
 
     private FirebaseStorage storage = FirebaseStorage.getInstance();
-    private StorageReference storageRef, imagesRef;
+    private StorageReference storageRef;
 
     private DatabaseReference mDatabase;
     private String databaseURL = "https://shootit-886f2-default-rtdb.europe-west1.firebasedatabase.app/";
@@ -91,11 +76,12 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
         backButton = (Button) findViewById(R.id.back_button);
         imageContainer = (LinearLayout) findViewById(R.id.imageContainer);
 
+        // When add photo button is pressed
         addPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                // Type of content user can select
+                // Get an image from gallery
                 mGetContent.launch("image/*");
             }
         });
@@ -120,12 +106,10 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
                         } else {
 
                             Toast.makeText(getApplicationContext(), "Please move the marker to your location", Toast.LENGTH_LONG).show();
-
                         }
                     } else {
 
                         Toast.makeText(getApplicationContext(), "Description too long or short", Toast.LENGTH_LONG).show();
-
                     }
                 } else {
 
@@ -134,6 +118,7 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
             }
         });
 
+        // If back button pressed finish activity
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -142,6 +127,7 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
             }
         });
 
+        // For fetching a user-selected image from gallery
         mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
                 new ActivityResultCallback<Uri>() {
                     @Override
@@ -161,6 +147,7 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
     }
 
 
+    // Add photo to screen
     private void addPhoto(Uri uri) {
 
         if (uri != null){
@@ -173,6 +160,7 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
     }
 
 
+    // Push new point and details to firebase
     public boolean pushToDatabase(ShootLocation newLocation) {
 
         StringBuilder sb = new StringBuilder();
@@ -196,6 +184,7 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
     }
 
 
+    // Upload photos to firestore
     public void uploadPhotos(List<Uri> photosList, String locationId) {
 
         // Gather Uris for images
@@ -223,15 +212,14 @@ public class CreatePointActivity extends AppCompatActivity implements OnMapReady
                         String url = task.getResult().toString();
                         System.out.println("Image uploaded");
                         mDatabase.child("locations").child(locationId).child("images").child(url.replaceAll("[^a-zA-Z0-9]", "")).setValue(url);
-                    } else {
-                        // Handle failures
-                        // ...
                     }
                 }
             });
         });
     }
 
+
+    // Initialise map fragment
     @Override
     public void onMapReady(GoogleMap tempMap) {
 
