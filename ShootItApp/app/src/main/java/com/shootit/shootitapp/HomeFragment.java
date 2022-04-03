02e21de65dd7fb6ce71a1b83471f2c1e139ac3da
sean-mcgregor <worksmcg@gmail.com>
2022-04-03 23:35:23 +0100

    Added a toast for connection issues

--- ShootItApp/app/src/main/java/com/shootit/shootitapp/HomeFragment.java
+++ ShootItApp/app/src/main/java/com/shootit/shootitapp/HomeFragment.java
@@ -204,7 +204,7 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.d("Crash", "Volle failed");
-                        error.printStackTrace();
+                        Toast.makeText(getContext(),"Connection issues. Please try again later.", Toast.LENGTH_LONG).show();
                     }
                 });
 
